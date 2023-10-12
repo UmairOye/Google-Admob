146140a5b8c39b63f1fa9f51721ca1d6139a0748
UmairOye <120459335+UmairOye@users.noreply.github.com>
2023-10-12 15:43:53 +0500

    OpenAd added

--- app/src/main/java/com/example/admoballadsimplementation/application/AdsApplication.kt
+++ app/src/main/java/com/example/admoballadsimplementation/application/AdsApplication.kt
@@ -5,8 +5,11 @@ import com.google.android.gms.ads.MobileAds
 
 
 class AdsApplication: Application() {
+    private var appOpenManager: AppOpenManager? = null
+
     override fun onCreate() {
         super.onCreate()
+        appOpenManager =  AppOpenManager(this)
         MobileAds.initialize(
             this
         ) { }
